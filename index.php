9c1d96a695ce0899f6da9077c03c8ca7566e2252
Jonas Heinrich <onny@project-insanity.org>
2013-01-09 07:21:42 +0000

    Validating 'from' and 'to' get-parameter

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 #	- mobile ui
 #	- dropdown menu für sprachen
 #	- suchen in beide richtungen
+#	- escape wrong input (security)
 
 # ABOUT:
 #	- http://www.apertium.org/ Ist frei, hat dics, lässt sich aber nicht benutzerfreundlich im Wiki-sinne erweitern
@@ -27,8 +28,207 @@
 -->
 
 <?php
+$languageCodes = array(
+ "aa" => "Afar",
+ "ab" => "Abkhazian",
+ "ae" => "Avestan",
+ "af" => "Afrikaans",
+ "ak" => "Akan",
+ "am" => "Amharic",
+ "an" => "Aragonese",
+ "ar" => "Arabic",
+ "as" => "Assamese",
+ "av" => "Avaric",
+ "ay" => "Aymara",
+ "az" => "Azerbaijani",
+ "ba" => "Bashkir",
+ "be" => "Belarusian",
+ "bg" => "Bulgarian",
+ "bh" => "Bihari",
+ "bi" => "Bislama",
+ "bm" => "Bambara",
+ "bn" => "Bengali",
+ "bo" => "Tibetan",
+ "br" => "Breton",
+ "bs" => "Bosnian",
+ "ca" => "Catalan",
+ "ce" => "Chechen",
+ "ch" => "Chamorro",
+ "co" => "Corsican",
+ "cr" => "Cree",
+ "cs" => "Czech",
+ "cu" => "Church Slavic",
+ "cv" => "Chuvash",
+ "cy" => "Welsh",
+ "da" => "Danish",
+ "de" => "German",
+ "dv" => "Divehi",
+ "dz" => "Dzongkha",
+ "ee" => "Ewe",
+ "el" => "Greek",
+ "en" => "English",
+ "eo" => "Esperanto",
+ "es" => "Spanish",
+ "et" => "Estonian",
+ "eu" => "Basque",
+ "fa" => "Persian",
+ "ff" => "Fulah",
+ "fi" => "Finnish",
+ "fj" => "Fijian",
+ "fo" => "Faroese",
+ "fr" => "French",
+ "fy" => "Western Frisian",
+ "ga" => "Irish",
+ "gd" => "Scottish Gaelic",
+ "gl" => "Galician",
+ "gn" => "Guarani",
+ "gu" => "Gujarati",
+ "gv" => "Manx",
+ "ha" => "Hausa",
+ "he" => "Hebrew",
+ "hi" => "Hindi",
+ "ho" => "Hiri Motu",
+ "hr" => "Croatian",
+ "ht" => "Haitian",
+ "hu" => "Hungarian",
+ "hy" => "Armenian",
+ "hz" => "Herero",
+ "ia" => "Interlingua (International Auxiliary Language Association)",
+ "id" => "Indonesian",
+ "ie" => "Interlingue",
+ "ig" => "Igbo",
+ "ii" => "Sichuan Yi",
+ "ik" => "Inupiaq",
+ "io" => "Ido",
+ "is" => "Icelandic",
+ "it" => "Italian",
+ "iu" => "Inuktitut",
+ "ja" => "Japanese",
+ "jv" => "Javanese",
+ "ka" => "Georgian",
+ "kg" => "Kongo",
+ "ki" => "Kikuyu",
+ "kj" => "Kwanyama",
+ "kk" => "Kazakh",
+ "kl" => "Kalaallisut",
+ "km" => "Khmer",
+ "kn" => "Kannada",
+ "ko" => "Korean",
+ "kr" => "Kanuri",
+ "ks" => "Kashmiri",
+ "ku" => "Kurdish",
+ "kv" => "Komi",
+ "kw" => "Cornish",
+ "ky" => "Kirghiz",
+ "la" => "Latin",
+ "lb" => "Luxembourgish",
+ "lg" => "Ganda",
+ "li" => "Limburgish",
+ "ln" => "Lingala",
+ "lo" => "Lao",
+ "lt" => "Lithuanian",
+ "lu" => "Luba-Katanga",
+ "lv" => "Latvian",
+ "mg" => "Malagasy",
+ "mh" => "Marshallese",
+ "mi" => "Maori",
+ "mk" => "Macedonian",
+ "ml" => "Malayalam",
+ "mn" => "Mongolian",
+ "mr" => "Marathi",
+ "ms" => "Malay",
+ "mt" => "Maltese",
+ "my" => "Burmese",
+ "na" => "Nauru",
+ "nb" => "Norwegian Bokmal",
+ "nd" => "North Ndebele",
+ "ne" => "Nepali",
+ "ng" => "Ndonga",
+ "nl" => "Dutch",
+ "nn" => "Norwegian Nynorsk",
+ "no" => "Norwegian",
+ "nr" => "South Ndebele",
+ "nv" => "Navajo",
+ "ny" => "Chichewa",
+ "oc" => "Occitan",
+ "oj" => "Ojibwa",
+ "om" => "Oromo",
+ "or" => "Oriya",
+ "os" => "Ossetian",
+ "pa" => "Panjabi",
+ "pi" => "Pali",
+ "pl" => "Polish",
+ "ps" => "Pashto",
+ "pt" => "Portuguese",
+ "qu" => "Quechua",
+ "rm" => "Raeto-Romance",
+ "rn" => "Kirundi",
+ "ro" => "Romanian",
+ "ru" => "Russian",
+ "rw" => "Kinyarwanda",
+ "sa" => "Sanskrit",
+ "sc" => "Sardinian",
+ "sd" => "Sindhi",
+ "se" => "Northern Sami",
+ "sg" => "Sango",
+ "si" => "Sinhala",
+ "sk" => "Slovak",
+ "sl" => "Slovenian",
+ "sm" => "Samoan",
+ "sn" => "Shona",
+ "so" => "Somali",
+ "sq" => "Albanian",
+ "sr" => "Serbian",
+ "ss" => "Swati",
+ "st" => "Southern Sotho",
+ "su" => "Sundanese",
+ "sv" => "Swedish",
+ "sw" => "Swahili",
+ "ta" => "Tamil",
+ "te" => "Telugu",
+ "tg" => "Tajik",
+ "th" => "Thai",
+ "ti" => "Tigrinya",
+ "tk" => "Turkmen",
+ "tl" => "Tagalog",
+ "tn" => "Tswana",
+ "to" => "Tonga",
+ "tr" => "Turkish",
+ "ts" => "Tsonga",
+ "tt" => "Tatar",
+ "tw" => "Twi",
+ "ty" => "Tahitian",
+ "ug" => "Uighur",
+ "uk" => "Ukrainian",
+ "ur" => "Urdu",
+ "uz" => "Uzbek",
+ "ve" => "Venda",
+ "vi" => "Vietnamese",
+ "vo" => "Volapuk",
+ "wa" => "Walloon",
+ "wo" => "Wolof",
+ "xh" => "Xhosa",
+ "yi" => "Yiddish",
+ "yo" => "Yoruba",
+ "za" => "Zhuang",
+ "zh" => "Chinese",
+ "zu" => "Zulu"
+);
 if(isset($_GET['from']) & isset($_GET['to']) & isset($_GET['q'])){
-  echo "Wenn from gültig und Teil des ISO-Arrays, dann der Variable from_lang zuweisen, die dann in das Input-Feld geschrieben wird. Wenn nicht, dann Standardval en in die Var schreiben";
+	if(isset($languageCodes[$_GET['from']])){
+		$from = $_GET['from'];
+	} else {
+		// passed language code is not correct, lets fallback
+		// to the default 
+		$from = "en";
+	}
+	if(isset($languageCodes[$_GET['to']])){
+		$to = $_GET['to'];
+	} else {
+		// passed language code is not correct, lets fallback
+		// to the default 
+		$to = "de";
+	}
 }
 ?>
 
@@ -51,8 +251,8 @@ if(isset($_GET['from']) & isset($_GET['to']) & isset($_GET['q'])){
       </div>
       <div id="right">
       <form class="form-inline">
-	  <input type="text" id="from" class="lang" value="en">
-	  <input type="text" id="to" class="lang" value="de">
+	  <input type="text" id="from" class="lang" value="<?=$from?>">
+	  <input type="text" id="to" class="lang" value="<?=$to?>">
 	  <input type="text" id="word" class="search-query" value="<?php echo $_GET['q'] ?>">
 	  <button class="btn btn-primary">translate</button>
       </form>
